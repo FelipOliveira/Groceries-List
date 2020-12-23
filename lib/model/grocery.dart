e0a01f2d9b8dc04ccf93651a89c47dcc5f1491e3
FelipOliveira <sem.ideias.na.hora@gmail.com>
2020-12-23 16:09:24 -0300

    database added

--- lib/model/grocery.dart
+++ lib/model/grocery.dart
@@ -1,7 +1,24 @@
 class Grocery {
   final int id;
-  final String item;
+  final String itemName;
+  final int quantity;
   final bool isBought;
 
-  Grocery({this.id, this.item, this.isBought});
+  Grocery({this.id, this.quantity, this.itemName, this.isBought});
+
+  factory Grocery.fromJson(Map<String, dynamic> data) {
+    return Grocery(
+      id: data['id'],
+      itemName: data['itemName'],
+      quantity: data['quantity'],
+      isBought: data['isBought'] == 0 ? false : true,
+    );
+  }
+
+  Map<String, dynamic> toDatabaseJson() => {
+        "id": this.id,
+        "itemName": this.itemName,
+        "quantity": this.quantity,
+        "isBought": this.isBought == false ? 0 : 1,
+      };
 }
